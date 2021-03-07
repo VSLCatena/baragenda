d2f49483d77f5683d724fed8ce46cd1c697f09df
Kipjr <martijn9873@hotmail.com>
2021-03-07 20:36:31 +0000

    Make event mass-fillable

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+		'shift_id','title','description','datetime_start','datetime_end','recurring_start','recurring_end','rrule','all_day','location_id','committee_id','attendees','status','google_calendar_id','google_event_id','google_parent_event_id','google_updated','updated_by'
+	 ];
+	 
 	public function getUser(){
 		return $this->hasMany('App\Models\User');
     }
