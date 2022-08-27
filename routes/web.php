cf5aabe1a7abe4ed6f63a1f9919df9ad16133899
Kipjr <12066560+Kipjr@users.noreply.github.com>
2022-08-27 21:19:10 +0200

    Add: Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+use Illuminate\Support\Str;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +19,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::match(['get', 'post'], '/login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
+Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('azure')->redirect();
+});
+Route::get('/auth/callback', function () {
+    $azureUser = Socialite::driver('azure')->user();
+
+    $user = User::updateOrCreate([
+        'username' => Str::before($azureUser->email,'@')
+    ], [
+        'username' => Str::before($azureUser->email,'@'),
+       # 'email' => $azureUser->email,
+       # 'azure_token' => $azureUser->token,
+       # 'azure_refresh_token' => $azureUser->refreshToken,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/home');
 });
